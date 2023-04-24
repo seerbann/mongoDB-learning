ed4ac234e0afe66ccc0b2f586e35ae088c46f409
seerbann <serbypwp@yahoo.com>
2023-04-24 19:05:51 +0300

    backend

--- carshop/src/App.js
+++ carshop/src/App.js
@@ -1,24 +1,45 @@
-import logo from "./logo.svg";
-import "./App.css";
-//edit
+import { useState } from "react";
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    let result = await fetch("http://localhost:5000/register", {
+      method: "post",
+      body: JSON.stringify({ name, email }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    result = await result.json();
+    console.warn(result);
+    if (result) {
+      alert("Data saved succesfully");
+      setEmail("");
+      setName("");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>This is React WebApp </h1>
+      <form action="">
+        <input
+          type="text"
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit" onClick={handleOnSubmit}>
+          submit
+        </button>
+      </form>
+    </>
   );
 }
 
