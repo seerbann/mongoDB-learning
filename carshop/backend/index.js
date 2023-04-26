3317ed98554e282ab62491f1b79bcff678352006
seerbann <serbypwp@yahoo.com>
2023-04-26 13:47:25 +0300

    tutorial phase

--- carshop/backend/index.js
+++ carshop/backend/index.js
@@ -1,65 +1,28 @@
-// To connect with your mongoDB database
-const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb://localhost:27017/",
-  {
-    dbName: "yourDB-name",
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) =>
-    err ? console.log(err) : console.log("Connected to yourDB-name database")
-);
+const { MongoClient } = require("mongodb");
 
-// Schema for users of app
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
-const User = mongoose.model("users", UserSchema);
-User.createIndexes();
+async function main() {
+  const uri =
+    "mongodb+srv://demo:demo@cluster0.zqxt72o.mongodb.net/?retryWrites=true&w=majority";
 
-// For backend and express
-const express = require("express");
-const app = express();
-const cors = require("cors");
-console.log("App listen at port 5000");
-app.use(express.json());
-app.use(cors());
-app.get("/", (req, resp) => {
-  resp.send("App is Working");
-  // You can check backend is working or not by
-  // entering http://loacalhost:5000
+  const client = new MongoClient(uri);
 
-  // If you see App is working means
-  // backend working properly
-});
-
-app.post("/register", async (req, resp) => {
   try {
-    const user = new User(req.body);
-    let result = await user.save();
-    result = result.toObject();
-    if (result) {
-      delete result.password;
-      resp.send(req.body);
-      console.log(result);
-    } else {
-      console.log("User already register");
-    }
+    await client.connect();
+    await listDatabases(client);
   } catch (e) {
-    resp.send("Something Went Wrong");
+    console.error(e);
+  } finally {
+    await client.close();
   }
-});
-app.listen(5000);
+}
+
+main().catch(console.error);
+
+async function listDatabases(client) {
+  const databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => {
+    console.log(`- ${db.name}`);
+  });
+}
